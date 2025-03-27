81f9ed9d119f29a79652f7cad3d6c507385efa5a
Justin Kasteleijn <justin.kasteleijn@gmail.com>
2025-03-28 00:03:45 +0100

    Took me WAAAAAYYYY to long to figure out the input could consist of multiple brackets ... so sad. Added a comment for now time to sleep and try again tomorrow.

---
+++ 2019/Day7/day7.hs
@@ -0,0 +1,6 @@
+-- Consider that one string can consist of multiple brackets
+-- (e.g. "ewyzepihewftallbppp[dmusynkrlcjtjymkzd]kjnubfdogkyyklwtoh[fralfdypbjeheiurvnf]cmdkpuvqorhbnrjhcus[nfrvtakzephycjks]bhabquktacxskzn") == 1
+
+main :: IO ()
+main = do 
+  print 5
